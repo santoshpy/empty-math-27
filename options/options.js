2791ff4fc778c15cd61b25be6d3a5e10c4892bba
Santosh Purbey <purbey.santosh@hotmail.com>
2023-08-25 19:16:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "empty-math-27",
+  url: "https://empty-math-27.botics.co",
+  api: "https://empty-math-27.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
